ef9a52827b511bc8019f29f5e5f81540d081f664
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-27 15:40:05 +0900

    Looking at a basic Merkle Tree which has a transaction hash and parent nodes

---
+++ algorithms/merkle_tree.py
@@ -0,0 +1,15 @@
+import hashlib
+from typing import List
+
+def sha256():
+    pass
+
+class MerkleTree:
+    def __init__(self) -> None:
+        pass
+
+    def build_merkle_root():
+        pass
+
+if __name__ == "__main__":
+    pass

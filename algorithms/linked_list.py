7c13709b388d2134b2796df4de0b1ecd51b89b82
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-27 16:00:29 +0900

    Looking how at how a linked list algo stores blocks on a blockchain. Each block points to the previous one

---
+++ algorithms/linked_list.py
@@ -0,0 +1,26 @@
+import hashlib 
+import time 
+
+class Block:
+    def __init__(self) -> None:
+        pass
+
+    def calculate_hash():
+        pass
+
+class Blockchain():
+    def __init__(self) -> None:
+        pass
+
+    def create_genesis_block():
+        pass
+
+    def add_block():
+        pass
+
+    def is_valid():
+        pass
+
+
+if __name__ == "__main__":
+    pass

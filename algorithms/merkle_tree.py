ebd24c204183a2c6cfc9d8ff3d7bf1aa5f9ff237
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-28 11:50:12 +0900

    Importin the hash_data function from the hashing algo file & refacotring the merkle_tree class

--- algorithms/merkle_tree.py
+++ algorithms/merkle_tree.py
@@ -1,29 +1,13 @@
-import hashlib
+from hashing import hash_data
 from typing import List
 
-def sha256(data: str) -> str:
-    return hashlib.sha256(data.encode('utf-8')).hexdigest()
 
 class MerkleTree:
     def __init__(self, transactions: List[str]):
-        self.transactions = transactions
-        self.root = self.build_merkle_root(transactions)
+        pass
 
     def build_merkle_root(self, tx_list: List[str]) -> str:
-        if not tx_list:
-            return ""
-
-        level = [sha256(tx) for tx in tx_list]
-
-        while len(level) > 1:
-            if len(level) % 2 != 0:
-                level.append(level[-1])
-            level = [sha256(level[i] + level[i+1]) for i in range(0, len(level), 2)]
-
-        return level
+        pass
 
 if __name__ == "__main__":
-    txs = ["tx1", "tx2", "tx3", "tx4"]
-    tree = MerkleTree(txs)
-    print("Transactions: ", txs)
-    print("Merkle Root: ", tree.root)
+    pass

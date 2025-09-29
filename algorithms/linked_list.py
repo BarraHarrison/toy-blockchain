c03f97170fa54f0fee77201c5da5897be8b71eb4
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-29 10:20:42 +0900

    Block's ID comes from the timestamp, data and the previous hash

--- algorithms/linked_list.py
+++ algorithms/linked_list.py
@@ -2,14 +2,21 @@ from hashing import hash_data
 import time 
 
 class Block:
-    def __init__(self, index, data, prev_hash):
-        pass
+    def __init__(self, data, prev_hash="0" * 64):
+        self.timestamp = time.time()
+        self.data = data
+        self.prev_hash = prev_hash
+        self.hash = self.compute_hash()
 
     def compute_hash(self):
-        pass
+        block_string = f"{self.timestamp}{self.data}{self.prev_hash}"
+        return hash_data(block_string, algo="sha256")
 
     def __repr__(self):
-        pass
+        return (f"Block(Hash: {self.hash[:12]}...,)"
+                f"Prev: {self.prev_hash[:12]}...,"
+                f"Data: {self.data}")
+
 
 class Blockchain():
     def __init__(self):

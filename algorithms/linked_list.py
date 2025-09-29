e5ea551d091acd8a5d8e424c6d4ddc45844bbced
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-29 10:13:35 +0900

    Representing each block as a node in the linked list

--- algorithms/linked_list.py
+++ algorithms/linked_list.py
@@ -1,48 +1,31 @@
-import hashlib 
+from hashing import hash_data
 import time 
 
 class Block:
     def __init__(self, index, data, prev_hash):
-        self.index = index 
-        self.timestamp = time.time()
-        self.data = data
-        self.prev_hash = prev_hash
-        self.hash = self.calculate_hash()
+        pass
 
-    def calculate_hash(self):
-        block_string = f"{self.index}{self.timestamp}{self.data}{self.prev_hash}"
-        
-        return hashlib.sha256(block_string.encode()).hexdigest()
+    def compute_hash(self):
+        pass
+
+    def __repr__(self):
+        pass
 
 class Blockchain():
     def __init__(self):
-        self.chain = [self.create_genesis_block()]
+        pass
 
     def create_genesis_block(self):
-        return Block(0, "Genesis Block", "0")
+        pass
 
     def add_block(self, data):
-        prev_block = self.chain[-1]
-        new_block = Block(len(self.chain), data, prev_block.hash)
-        self.chain.append(new_block)
+        pass
 
     def is_valid(self):
-        for i in range(1, len(self.chain)):
-            curr = self.chain[i]
-            prev = self.chain[i-1]
-            if curr.hash != curr.calculate_hash():
-                return False
-            if curr.prev_hash != prev.hash:
-                return False
-        return True
+        pass
 
+    def __repr__(self):
+        pass
 
 if __name__ == "__main__":
-    bc = Blockchain()
-    bc.add_block("Adam pays Bill 10 BTC")
-    bc.add_block("Bill pays Charles 6 BTC")
-
-    for block in bc.chain:
-        print(vars(block))
-
-    print("Blockchain valid: ", bc.is_valid())
+    pass

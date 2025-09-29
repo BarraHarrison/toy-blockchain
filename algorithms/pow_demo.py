8fd499db3d7637df1d76af466258abd00e2e1d0f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-09-29 10:55:09 +0900

    Proof of work file to show why tampering with the blockchain is expensive and time-consuming

---
+++ algorithms/pow_demo.py
@@ -0,0 +1,40 @@
+import time
+from hashing import hash_data
+
+class Block:
+    def __init__(self):
+        pass
+
+    def compute_hash(self):
+        pass
+
+    def mine(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class Blockchain:
+    def __init__(self):
+        pass
+
+    def create_genesis_block(self):
+        pass
+
+    def add_block(self):
+        pass
+
+    def is_valid(self):
+        pass
+
+    def print_chain(self):
+        pass
+
+    def re_mine_from(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    pass
